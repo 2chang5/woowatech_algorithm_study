331fb62a099f2c52e9349ca345e70c504fd3503b
2chang5 <chang5hustle@gmail.com>
2023-08-07 10:11:44 +0900

    7568

---
+++ 멧돼지/2주차/7568.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+data = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(n)]
+eachAns = 1
+ans = []
+
+for i in data:
+    for j in data:
+        if i[0] < j[0] and i[1] < j[1]:
+            eachAns += 1
+    ans.append(eachAns)
+    eachAns = 1
+
+print(ans, sep="")
+
+# 이거 틀린답인데 왜틀린지 모르겠어서 상의해봐야함
